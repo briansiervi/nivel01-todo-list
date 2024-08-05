b92264614c02509dc679ac9ff05f710d3aa0acb7
Brian <brianmazini@gmail.com>
2024-08-05 08:07:29 -0300

    adição da prancheta quado não há tarefas

--- src/components/TaskListComponents/TaskListBody.tsx
+++ src/components/TaskListComponents/TaskListBody.tsx
@@ -1,6 +1,7 @@
 import { Task, TaskProps } from "../TaskComponents/Task";
 import styles from "./TaskListBody.module.css";
 import { v4 as uuidv4 } from "uuid";
+import { ClipboardText } from "phosphor-react";
 
 interface TaskListBodyProps {
   tasks: TaskProps[];
@@ -14,19 +15,33 @@ export function TaskListBody({
   onCrossOutTask,
 }: TaskListBodyProps) {
   return (
-    <div className={styles.line}>
-      {tasks
-        .sort((x) => x.id)
-        .map((task) => {
-          return (
-            <Task
-              key={uuidv4()}
-              task={task}
-              onDeleteTask={onDeleteTask}
-              onCrossOutTask={onCrossOutTask}
-            />
-          );
-        })}
-    </div>
+    <>
+      {tasks.length > 0 ? (
+        <div className={styles.taskList}>
+          {tasks
+            .sort((x) => x.id)
+            .map((task) => {
+              return (
+                <Task
+                  key={uuidv4()}
+                  task={task}
+                  onDeleteTask={onDeleteTask}
+                  onCrossOutTask={onCrossOutTask}
+                />
+              );
+            })}
+        </div>
+      ) : (
+        <div className={styles.emptyList}>
+          <ClipboardText size={56} weight="duotone" />
+          <p className={styles.emptyListTitle}>
+            Você ainda não tem tarefas cadastradas
+          </p>
+          <p className={styles.emptyListBody}>
+            Crie tarefas e organize seus itens a fazer
+          </p>
+        </div>
+      )}
+    </>
   );
 }
